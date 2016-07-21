ae76725e299389fee5c6c16366854237eb3fd211
brijal92 <brijal92@gmail.com>
2016-07-20 23:57:44 -0400

    Add files via upload

---
+++ refine.R
@@ -0,0 +1,33 @@
+# Load the data in RStudio
+library(tidyr)
+library(dplyr)
+# Clean up brand names
+refine_original$company <- tolower(refine_original$company)
+str(refine_original)
+#  Separate product code and number
+refine1 <- mutate(refine_original, Company = substr(refine_original$company, 1 , stop = 1))
+refine1$Company <- grep("^[p]", "philips", refine1$Company)
+refine1$Company <- grep("^[a]", "akzo", refine1$Company)
+refine1$Company <- grep("^[u]", "unilever", refine1$Company)
+refine1$Company <- grep("^[v]", "van houten", refine1$Company)
+refine1 <- select(refine1, -company)
+refine1 <- separate(refine1, Product.code.number, c("product_code", "product_number"), sep = "-")
+# Add product catogeries
+refine1 <- mutate(refine1, product_category = product_code)
+refine1$product_category <- grep("^[p|f]", "Smartphone", refine1$product_category)
+refine1$product_category <- grep("^[v]", "TV", refine1$product_category)
+refine1$product_category <- grep("^[x]", "Laptop", refine1$product_category)
+refine1$product_category <- grep("^[q]", "Tablet", refine1$product_category)
+#Add full address for geocoding
+refine1 <- unite(refine1, full_address, address : country, sep = "," )
+refine1 <- mutate(refine1, company_philips = ifelse(Company == "philips", 1, 0))
+refine1 <- mutate(refine1, company_akzo = ifelse(Company == "akzo", 1, 0))
+refine1 <- mutate(refine1, company_unilever = ifelse(Company == "unilever", 1, 0))
+refine1 <- mutate(refine1, company_van_houten = ifelse(Company == "van houten", 1, 0))
+refine1 <- mutate(refine1, product_smartphone = ifelse(product_category == "Smartphone", 1, 0))
+refine1 <- mutate(refine1, product_laptop = ifelse(product_category == "Laptop", 1, 0))
+refine1 <- mutate(refine1, product_tv = ifelse(product_category == "TV", 1, 0))
+refine1 <- mutate(refine1, product_tablet = ifelse(product_category == "Tablet", 1, 0))
+refine_clean <- refine1
+write.csv(refine_clean, file = "refine_clean.csv")
+
---
+++ titanic.R
@@ -0,0 +1,45 @@
+library(dplyr)
+library(tidyr)
+
+# Reading File from CSV
+rawcsv <- read.csv("titanic_original.csv",header = TRUE)
+# changing  data frame to tbl
+rawcsvtbl <- tbl_df(rawcsv)
+# embarkation
+# Find the missing values and replace them with S.
+vemb <- as.character(rawcsvtbl$embarked)
+ember_place <- function(item){
+  if(item=="") (item=="S")
+  else{item=item}
+}
+vemb <- vapply(emb,ember_place,character(1))
+rawcsvtbl$embarked <- vemb
+# age - find mean of values,poplulation with empty values
+eage <- as.numeric(rawcsvtbl$age)
+mean_age <- mean(eage,na.rm = TRUE)
+age_replace <- function(item,age){
+  if(is.na(item)){item=age}
+  else{item = item}
+}
+eage <- sapply(eage, age_replace,mean_age)
+rawcsvtbl$age <- eage
+# life boat - empty value with na
+vlife <- as.character(rawcsvtbl$boat)
+life_replace <-function(item){
+  if (item==""){item = "NA"}
+  else{item=item}
+}
+vlife <- sapply(vlife,life_replace)
+rawcsvtbl$boat <- vlife
+# if cabin value  has_cabin_number which has 1
+vcabin <- as.character(rawcsvtbl$cabin)
+cabin_replace <-function(item) {
+  if (item==""){item = "1"}
+  else{item=0}
+}
+vcabin <- vapply(vcabin,cabin_replace,numeric(1))
+rawcsvtbl <- cbind(rawcsvtbl,vcabin)
+# removing last raw,
+rawcsvtbl <- rawcsvtbl[-1310,]
+# out put
+write.csv(rawcsvtbl,file = "titanic_clean.csv",row.names=FALSE)
